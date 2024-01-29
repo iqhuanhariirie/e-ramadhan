f4919c5e1fa1100be63a486c973d152d272883ed
Asyraaf16 <mohdasyraa@gmail.com>
2024-01-29 19:33:58 +0800

    acap

--- resources/lang/ms/app.php
+++ resources/lang/ms/app.php
@@ -7,7 +7,7 @@ return [
     'yes' => 'Ya',
     'label' => 'Label',
     'action' => 'Pilihan',
-    'welcome' => 'Assalamualaikum semua',
+    'welcome' => 'Assalamualaikum',
     'active' => 'Aktif',
     'inactive' => 'Tidak Aktif',
     'show_detail_title' => 'Lihat detail :type :name',
